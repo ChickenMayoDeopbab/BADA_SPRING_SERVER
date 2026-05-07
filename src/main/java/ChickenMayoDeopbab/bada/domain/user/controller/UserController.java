12e49801a36e713baac6e57a71596cf0ef3e8e0f
ParkJunSeok <jdksla0129@gmail.com>
2026-05-07 19:31:08 +0900

    refactor: api명세에 맞게 api를 수정

--- src/main/java/ChickenMayoDeopbab/bada/domain/user/controller/UserController.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/user/controller/UserController.java
@@ -5,9 +5,11 @@ import ChickenMayoDeopbab.bada.domain.user.service.UserService;
 import ChickenMayoDeopbab.bada.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
