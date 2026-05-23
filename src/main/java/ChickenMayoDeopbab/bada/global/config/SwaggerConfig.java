0c7f7bdf991d4fd9a4897287da916406c530b780
이도연 <ldy2009aa@gmail.com>
2026-05-23 20:32:28 +0900

    feat : move server url to env

--- src/main/java/ChickenMayoDeopbab/bada/global/config/SwaggerConfig.java
+++ src/main/java/ChickenMayoDeopbab/bada/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +15,9 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${app.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         SecurityScheme securityScheme = new SecurityScheme()
@@ -29,7 +33,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Local"),
-                        new Server().url("http://3.39.219.113:8080").description("Production")
+                        new Server().url(serverUrl).description("Production")
                 ))
                 .components(new Components().addSecuritySchemes(HttpHeaders.AUTHORIZATION, securityScheme))
                 .addSecurityItem(securityRequirement)
