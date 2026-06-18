3a92742f19971d6d0a7a68ffecb2bb46fffcda03
junha0729 <junha0729@dgsw.hs.kr>
2026-06-18 17:43:29 +0900

    feat: 세션 예외처리

--- src/main/java/ChickenMayoDeopbab/bada/domain/session/exception/SessionStatusCode.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/session/exception/SessionStatusCode.java
@@ -1,4 +1,17 @@
 package ChickenMayoDeopbab.bada.domain.session.exception;
 
-public enum SessionStatusCode {
+import ChickenMayoDeopbab.bada.global.exception.statuscode.StatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SessionStatusCode implements StatusCode {
+    SCENARIO_NOT_FOUND(HttpStatus.NOT_FOUND, "SESSION_001", "시나리오를 찾을 수 없습니다."),
+    REDIS_WRITE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SESSION_002", "세션 저장에 실패했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
