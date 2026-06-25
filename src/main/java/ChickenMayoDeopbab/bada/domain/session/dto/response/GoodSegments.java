a46851d83bfe59c201d9dc26f97d918bcd9f84f2
ParkJunSeok <jdksla0129@gmail.com>
2026-06-25 17:09:02 +0900

    fix: GoodSegments fastApi와 맞게 수정

--- src/main/java/ChickenMayoDeopbab/bada/domain/session/dto/response/GoodSegments.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/session/dto/response/GoodSegments.java
@@ -1,11 +1,6 @@
 package ChickenMayoDeopbab.bada.domain.session.dto.response;
 
 public record GoodSegments(
-        Role role,
-        String text
-) {
-    public enum Role {
-        user,
-        assistant
-    }
-}
+        float start,
+        float end
+) { }
