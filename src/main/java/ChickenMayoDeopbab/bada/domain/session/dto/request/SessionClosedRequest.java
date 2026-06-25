9dbbce5b027c8e209b70533f4b06ed6ed09f92f0
이도연 <ldy2009aa@gmail.com>
2026-06-25 18:09:31 +0900

    fix: change method name

--- src/main/java/ChickenMayoDeopbab/bada/domain/session/dto/request/SessionClosedRequest.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/session/dto/request/SessionClosedRequest.java
@@ -1,7 +1,5 @@
 package ChickenMayoDeopbab.bada.domain.session.dto.request;
 
-import ChickenMayoDeopbab.bada.domain.session.dto.response.GoodSegments;
-import ChickenMayoDeopbab.bada.domain.session.dto.response.SessionClosedResponse;
 import ChickenMayoDeopbab.bada.domain.session.enums.EndReason;
 import ChickenMayoDeopbab.bada.domain.session.model.GoodSegment;
 import ChickenMayoDeopbab.bada.domain.session.model.TranscriptTurn;
