8162b99509852f37104e6f32bd66c6551e3bd678
ParkJunSeok <jdksla0129@gmail.com>
2026-06-28 18:05:20 +0900

    feat: feedback 반환 api에 시나리오 이름 및 총 플레이 타임 출력

--- src/main/java/ChickenMayoDeopbab/bada/domain/trainingrecord/dto/response/FeedbackResponse.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/trainingrecord/dto/response/FeedbackResponse.java
@@ -2,15 +2,24 @@ package ChickenMayoDeopbab.bada.domain.trainingrecord.dto.response;
 
 import ChickenMayoDeopbab.bada.domain.session.model.GoodSegment;
 import ChickenMayoDeopbab.bada.domain.trainingrecord.entity.TrainingRecord;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 
 public record FeedbackResponse(
+        String scenarioName,
+        @JsonFormat(pattern = "HH:mm:ss")
+        LocalTime trainingTime,
         List<GoodSegment> goodSegments,
         String recordingUrl
 ) {
     public static FeedbackResponse of(TrainingRecord trainingRecord, List<GoodSegment> goodSegments) {
         return new FeedbackResponse(
+                trainingRecord.getScenarioName(),
+                LocalTime.ofSecondOfDay
+                        (Duration.between(trainingRecord.getStartedAt(), trainingRecord.getEndedAt()).getSeconds()),
                 goodSegments,
                 trainingRecord.getRecordingUrl()
         );
