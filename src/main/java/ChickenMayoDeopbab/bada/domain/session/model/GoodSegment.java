4ff9548d6e44fc1a7cbe0f9a6b863cbc417c7365
ParkJunSeok <jdksla0129@gmail.com>
2026-07-05 15:14:03 +0900

    feat: add good_point to positive feedback response

--- src/main/java/ChickenMayoDeopbab/bada/domain/session/model/GoodSegment.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/session/model/GoodSegment.java
@@ -1,5 +1,7 @@
 package ChickenMayoDeopbab.bada.domain.session.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 // FastAPI가 전달하는 잘한 발화 구간(초 단위)
-public record GoodSegment(Double start, Double end) {
+public record GoodSegment(Double start, Double end, @JsonProperty("good_point") String goodPoint) {
 }
