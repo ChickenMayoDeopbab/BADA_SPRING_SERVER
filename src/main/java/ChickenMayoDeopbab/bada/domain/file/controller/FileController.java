617a8344351a3a01a860edee884ce831e3830bb0
ParkJunSeok <jdksla0129@gmail.com>
2026-07-06 22:48:29 +0900

    feat: add temporary file upload endpoint for s3 verification

--- src/main/java/ChickenMayoDeopbab/bada/domain/file/controller/FileController.java
+++ src/main/java/ChickenMayoDeopbab/bada/domain/file/controller/FileController.java
@@ -1,13 +1,17 @@
 package ChickenMayoDeopbab.bada.domain.file.controller;
 
+import ChickenMayoDeopbab.bada.domain.file.dto.response.FileUploadResponse;
 import ChickenMayoDeopbab.bada.domain.file.dto.response.GetUrlResponse;
+import ChickenMayoDeopbab.bada.domain.file.enumeration.FileType;
 import ChickenMayoDeopbab.bada.domain.file.service.FileService;
 import ChickenMayoDeopbab.bada.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/file")
@@ -19,5 +23,13 @@ public class FileController {
     public ApiResponse<GetUrlResponse> getPresignedUrl(@RequestParam("fileId") Long fileId) {
         String url = fileService.getUrl(fileId);
         return ApiResponse.ok(new GetUrlResponse(url));
+    }
+
+    // TODO(임시/테스트용): S3 IAM Role 업로드 확인용. 검증 후 제거.
+    @PostMapping
+    public ApiResponse<FileUploadResponse> upload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("fileType") FileType fileType) {
+        return ApiResponse.created(fileService.upload(file, fileType), "파일 업로드 성공");
     }
 }
